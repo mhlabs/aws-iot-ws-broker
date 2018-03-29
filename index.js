72b56e6b2e8bdc1d2b1f282a804e37b100fe19f2
Lars <lars@mathem.se>
2018-03-29 11:40:21 +0200

    birth of new version

--- index.js
+++ index.js
@@ -2,49 +2,28 @@
 
 const awsIot = require('aws-iot-device-sdk');
 
-module.exports = function (keysUrl) {
-    if (keysUrl === "") {
-        throw new Error("Url missing");
-    }
+module.exports = function (iotEndpoint) {
     var client;
     var iotTopics = [];
     var WS = {
-        connect: function(keysUrl) {
-            var xhr = createCORSRequest('GET', keysUrl);
-
-            xhr.send(null);
-
-            xhr.onreadystatechange = function () {
-                var DONE = 4;
-                var OK = 200;
-                if (xhr.readyState === DONE) {
-                    if (xhr.status === OK)
-                        var body = JSON.parse(xhr.responseText);
-                    console.log(body);
-                    console.log(body.region);
-
+        connect: function() {
                     client = awsIot.device({
-                        region: body.region,
+                        region: AWS.config.region,
                         protocol: 'wss',
-                        accessKeyId: body.accessKey,
-                        secretKey: body.secretKey,
-                        sessionToken: body.sessionToken,
+                        accessKeyId: AWS.credentials.AccessKeyId,
+                        secretKey: AWS.credentials.SecretKey,
+                        sessionToken: AWS.credentials.SessionToken,
                         port: 443,
-                        host: body.iotEndpoint
+                        host: iotEndpoint
                     });
 
                     client.on('connect', WS.onConnect);
-                    client.on('message', WS.onMessage);
+                    client.on('message', (x,y) => this.onMessage(x,y));
                     client.on('error', WS.onError);
                     client.on('reconnect', WS.onReconnect);
                     client.on('offline', WS.onOffline);
                     client.on('close', WS.onClose);
-                }
-                else {
-                    console.log('Error: ' + xhr.status);
-                }
-            }
-        },
+            },
 
         send: function(topic, message) {
             client.publish(topic, message);
@@ -62,7 +41,7 @@ module.exports = function (keysUrl) {
 
         onConnect: function() {
             for (var i = 0; i < iotTopics.length; i++) {
-                console.log("trying to connect to " + iotTopics[i])
+                console.log("trying to connect to " + iotTopics[i]);
                 WS.subscribe(iotTopics[i]);
             }
             iotTopics = [];
@@ -81,23 +60,7 @@ module.exports = function (keysUrl) {
         onOffline: function() { console.log('Offline'); }
     };
 
-    WS.connect(keysUrl);
+    WS.connect(iotEndpoint);
     return WS;
 
-
 }
-
-
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
-}
-
