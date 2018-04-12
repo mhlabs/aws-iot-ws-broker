8cb44dd1be88294fcdfbd8e56c063a7f2a4d83b7
Jan-Erik Lysander <jan-erik@lysander.se>
2018-04-12 12:59:17 +0200

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,23 @@
 'use strict';
-var expect = require('chai').expect;
-var AwsIot = require('../dist/index');
+import { expect } from "chai";
+import AwsIot from "../dist/index";
+import 'babel-polyfill';
 
-describe('AWS IOT websocket broker', function() {
-    it('should throw an error when no config is supplied', function() {
-        var result = true;
-        expect(result).to.equal(true);        
+
+describe('AWS IOT websocket broker', () => {
+    let awsIot;
+
+    beforeEach(() => {
+        awsIot = new AwsIot();
+    });
+
+    it('should throw an error when connecting without a config', () => {
+        expect(awsIot.connect.bind(awsIot)).to.throw(Error, 'No AWS Cognito credentials provided');        
+    });
+
+    it('should que up subscriptions before the client is ready', () => {
+        const topicName = 'some-topic';
+        awsIot.subscribe(topicName);
+        expect(awsIot.topics).to.include(topicName);
     });
 });
