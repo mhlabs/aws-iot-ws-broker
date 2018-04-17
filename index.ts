8e9f6244a0a97b157ed78fe5dda04e70f94de475
Jan-Erik Lysander <jan-erik@lysander.se>
2018-04-17 10:03:47 +0200

    Added interface for updating credentials

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import { device, DeviceOptions } from 'aws-iot-device-sdk';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-import AWS = require('aws-sdk');
-import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
+import { device, DeviceOptions } from "aws-iot-device-sdk";
+import { Subject } from "rxjs/Subject";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/map";
+import AWS = require("aws-sdk");
+import { Observable } from "rxjs/Observable";
+import { Observer } from "rxjs/Observer";
 
 export default class AwsIot {
   public readonly events = new Subject<IotEvent>();
 
-  private client: any;
+  private client!: device;
   private topics = new Array<string>();
 
-  constructor(private debugMode = false) { }
+  constructor(private debugMode = false) {}
 
   public connect(creds: AWS.CognitoIdentityCredentials, policyName: string) {
     if (!creds) {
-      throw new Error('No AWS Cognito credentials provided');
+      throw new Error("No AWS Cognito credentials provided");
     }
 
     const iot = new AWS.Iot();
@@ -28,29 +28,26 @@ export default class AwsIot {
 
     const principal = creds.identityId;
 
-    iot.attachPrincipalPolicy({ principal, policyName }, (policyErr) => {
+    iot.attachPrincipalPolicy({ principal, policyName }, policyErr => {
       if (policyErr) {
-        this.log('Error attaching policy', policyErr);
+        this.log("Error attaching policy", policyErr);
         return;
       }
 
       this.createDevice(iot, creds);
     });
-
   }
 
   private createDevice(iot: AWS.Iot, creds: AWS.CognitoIdentityCredentials) {
-
     iot.describeEndpoint({}, (err, data) => {
-
       if (err) {
-        this.log('Error getting endpoint address', err);
+        this.log("Error getting endpoint address", err);
         return;
       }
 
       const config: DeviceOptions = {
         region: AWS.config.region,
-        protocol: 'wss',
+        protocol: "wss",
         accessKeyId: creds.accessKeyId,
         secretKey: creds.secretAccessKey,
         sessionToken: creds.sessionToken,
@@ -62,27 +59,37 @@ export default class AwsIot {
       try {
         this.client = new device(config);
       } catch (deviceErr) {
-        this.log('Error creating device:', deviceErr);
+        this.log("Error creating device:", deviceErr);
         return;
       }
 
-      this.client.on('connect', () => this.onConnect());
-      this.client.on('message', (topic: string, message: any) => this.onMessage(topic, message));
-      this.client.on('error', () => this.onError());
-      this.client.on('reconnect', () => this.onReconnect());
-      this.client.on('offline', () => this.onOffline());
-      this.client.on('close', () => this.onClose());
+      this.client.on("connect", () => this.onConnect());
+      this.client.on("message", (topic: string, message: any) =>
+        this.onMessage(topic, message)
+      );
+      this.client.on("error", () => this.onError());
+      this.client.on("reconnect", () => this.onReconnect());
+      this.client.on("offline", () => this.onOffline());
+      this.client.on("close", () => this.onClose());
     });
   }
 
   public disconnect(): Observable<null> {
     return Observable.create((observer: Observer<null>) => {
       this.client.end(true, () => {
-        this.client = null;
         observer.next(null);
         observer.complete();
       });
-    })
+    });
+  }
+
+  public updateCredentials(credentials: AWS.CognitoIdentityCredentials) {
+    this.client.updateWebSocketCredentials(
+      credentials.accessKeyId,
+      credentials.secretAccessKey,
+      credentials.sessionToken,
+      credentials.expireTime
+    );
   }
 
   public send(topic: string, message: any) {
@@ -92,25 +99,25 @@ export default class AwsIot {
   public subscribe(topic: string) {
     if (this.client) {
       this.client.subscribe(topic);
-      this.log('Subscribed to topic:', topic);
+      this.log("Subscribed to topic:", topic);
     } else {
       this.topics.push(topic);
-      this.log('Deferring subscription of topic:', topic);
+      this.log("Deferring subscription of topic:", topic);
     }
   }
 
   public unsubscribe(topic: string) {
     if (this.client) {
       this.client.unsubscribe(topic);
-      this.log('Unubscribed from topic:', topic);
+      this.log("Unubscribed from topic:", topic);
     }
   }
 
   private onConnect() {
-    this.log('Connected');
+    this.log("Connected");
     this.events.next({ type: IotEventType.Connect });
     for (const topic of this.topics) {
-      this.log('Trying to connect to topic:', topic);
+      this.log("Trying to connect to topic:", topic);
       this.subscribe(topic);
     }
 
@@ -127,22 +134,22 @@ export default class AwsIot {
   }
 
   private onClose() {
-    this.log('Connection failed');
+    this.log("Connection failed");
     this.events.next({ type: IotEventType.Close });
   }
 
   private onError() {
-    this.log('Error');
+    this.log("Error");
     this.events.next({ type: IotEventType.Error });
   }
 
   private onReconnect() {
-    this.log('Reconnected');
+    this.log("Reconnected");
     this.events.next({ type: IotEventType.Reconnect });
   }
 
   private onOffline() {
-    this.log('Offline');
+    this.log("Offline");
     this.events.next({ type: IotEventType.Offline });
   }
 
@@ -153,7 +160,7 @@ export default class AwsIot {
   }
 }
 
-export { DeviceOptions } from 'aws-iot-device-sdk';
+export { DeviceOptions } from "aws-iot-device-sdk";
 
 export interface IotEvent {
   type: IotEventType;
@@ -162,10 +169,10 @@ export interface IotEvent {
 }
 
 export enum IotEventType {
-  Connect = 'connect',
-  Message = 'message',
-  Close = 'close',
-  Error = 'error',
-  Reconnect = 'reconnect',
-  Offline = 'offline'
+  Connect = "connect",
+  Message = "message",
+  Close = "close",
+  Error = "error",
+  Reconnect = "reconnect",
+  Offline = "offline"
 }
